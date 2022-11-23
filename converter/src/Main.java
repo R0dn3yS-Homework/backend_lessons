76d4e9369e15a15dda605e9e8177529061d91506
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-11-23 11:56:57 +0100

    Updates to converter

--- converter/src/Main.java
+++ converter/src/Main.java
@@ -1,31 +1,54 @@
 import java.util.Scanner;
-import static java.lang.Double.parseDouble;
+
 
 public class Main {
   public static void main(String[] args) {
+    convert();
+  }
+
+  public static void convert() {
     double usdValue = 1.03;
+    double jpyValue = 145.79;
+    double audValue = 1.55;
 
     Scanner sc = new Scanner(System.in);
 
-    System.out.print("Is your current value USD or EUR: ");
+    System.out.print("\nDo you want to convert to USD, JPY or AUD: ");
     String choice = sc.nextLine();
 
-    if (choice.equalsIgnoreCase("EUR")) {
-      System.out.print("\nPlease enter you current value in EUR: ");
-      double cur = sc.nextDouble();
+    switch (choice.toUpperCase()) {
+      case "JPY" -> {
+        System.out.print("\nPlease enter you current value in EUR: ");
+        double cur = sc.nextDouble();
 
-      String newValue = String.format("%.2f", cur * usdValue);
+        String newValue = String.format("%.2f", cur * jpyValue);
 
-      System.out.println("The value of " + cur + "EUR is " + newValue + "USD");
-    } else if (choice.equalsIgnoreCase("USD")) {
-      System.out.print("\nPlease enter you current value in USD: ");
-      double cur = sc.nextDouble();
+        System.out.println("The value of " + cur + "EUR is " + newValue + "JPY");
+      }
+      case "USD" -> {
+        System.out.print("\nPlease enter you current value in EUR: ");
+        double cur = sc.nextDouble();
 
-      String newValue = String.format("%.2f", cur / usdValue);
+        String newValue = String.format("%.2f", cur * usdValue);
 
-      System.out.println("The value of " + cur + "USD is " + newValue + "EUR");
-    } else {
-      System.out.println("That's not a valid choice.\nExiting...");
+        System.out.println("The value of " + cur + "EUR is " + newValue + "USD");
+      }
+      case "AUD" -> {
+        System.out.print("\nPlease enter you current value in EUR: ");
+        double cur = sc.nextDouble();
+
+        String newValue = String.format("%.2f", cur * audValue);
+
+        System.out.println("The value of " + cur + "EUR is " + newValue + "AUD");
+
+      }
+    }
+
+    System.out.print("\nDo you want to convert another value (y/N) ");
+    sc.nextLine();
+    String again = sc.nextLine();
+    if (again.equalsIgnoreCase("y")) {
+      convert();
     }
   }
 }
