ea17f7de990d1654bb3920968869ecca2891f830
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-11-25 09:27:21 +0100

    console.log

--- loops/src/Main.java
+++ loops/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-
+    console.log("test");
   }
 }
---
+++ loops/src/console.java
@@ -0,0 +1,5 @@
+public class console {
+  public static void log(String text) {
+    System.out.println(text);
+  }
+}
