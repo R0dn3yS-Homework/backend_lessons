8f8398ace94e562ad096a6d1d5fd69b5720b924e
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-11-25 11:42:56 +0100

    converter: overhaul

--- converter/src/Main.java
+++ converter/src/Main.java
@@ -1,43 +1,21 @@
 import java.util.Scanner;
 
-
 public class Main {
   public static void main(String[] args) {
     convert();
   }
 
   public static void convert() {
-    double usdValue = 1.03;
-    double jpyValue = 145.79;
-    double audValue = 1.55;
-    
-    String newValue = null;
-
     Scanner sc = new Scanner(System.in);
+    Result rate = getRate(sc);
+    Result conversionRate = convertTo(rate.getFirst(), sc);
 
-    System.out.print("\nDo you want to convert to USD, JPY or AUD: ");
-    String choice = sc.nextLine();
-
-    System.out.print("\nPlease enter you current value in EUR: ");
+    System.out.print("\nPlease enter you current value in " + rate.getSecond() + ": ");
     double cur = sc.nextDouble();
 
-    switch (choice.toUpperCase()) {
-      case "JPY" -> {
-        newValue = String.format("%.2f", cur * jpyValue);
-      }
-      case "USD" -> {
-        newValue = String.format("%.2f", cur * usdValue);
-      }
-      case "AUD" -> {
-        newValue = String.format("%.2f", cur * audValue);
-      }
-      default -> {
-        System.out.println("Invalid choice.\nExiting...");
-        System.exit(1);
-      }
-    }
+    String newValue = String.format("%.2f", cur * conversionRate.getFirst());
 
-    System.out.println("The value of " + cur + "EUR is " + newValue + choice.toUpperCase());
+    System.out.println("The value of " + cur + rate.getSecond() + " is " + newValue + conversionRate.getSecond());
 
     System.out.print("\nDo you want to convert another value (y/N) ");
     sc.nextLine();
@@ -49,5 +27,61 @@ public class Main {
       sc.close();
       System.exit(0);
     }
+  }
+
+  public static Result getRate(Scanner sc) {
+    double rate = 0;
+
+    System.out.print("\nWhat is your current currency: EUR, USD, JPY or AUD: ");
+    String choice = sc.nextLine();
+
+    switch (choice.toUpperCase()) {
+      case "EUR" -> {
+        rate = 1.0;
+      }
+      case "JPY" -> {
+        rate = 145.79;
+      }
+      case "USD" -> {
+        rate = 1.03;
+      }
+      case "AUD" -> {
+        rate = 1.55;
+      }
+      default -> {
+        System.out.println("Invalid choice.\nExiting...");
+        System.exit(1);
+      }
+    }
+
+    return new Result(rate, choice.toUpperCase());
+  }
+
+  public static Result convertTo(double rate, Scanner sc) {
+    double newRate = 0;
+
+    System.out.print("\nDo you want to convert to EUR, USD, JPY or AUD: ");
+    String choice = sc.nextLine();
+
+    switch (choice.toUpperCase()) {
+      case "EUR" -> {
+        newRate = 1.0 / rate;
+      }
+      case "JPY" -> {
+        newRate = 145.79 / rate;
+      }
+      case "USD" -> {
+        newRate = 1.03 / rate;
+      }
+      case "AUD" -> {
+        newRate = 1.55 / rate;
+      }
+      default -> {
+        System.out.println("Invalid choice.\nExiting...");
+        System.exit(1);
+      }
+    }
+
+    return new Result(newRate, choice.toUpperCase());
   }
 }
---
+++ converter/src/Result.java
@@ -0,0 +1,17 @@
+final class Result {
+  private final double first;
+  private final String second;
+
+  public Result(double first, String second) {
+    this.first = first;
+    this.second = second;
+  }
+
+  public double getFirst() {
+    return first;
+  }
+
+  public String getSecond() {
+    return second;
+  }
+}
